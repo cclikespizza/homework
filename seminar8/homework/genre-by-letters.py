d76fc01afda8783e8b5d3f08e43d1eb6230490fb
Elmira <cclikespizza@gmail.com>
2015-11-19 23:50:00 +0300

    a small bug fixed

--- seminar8/homework/genre-by-letters.py
+++ seminar8/homework/genre-by-letters.py
@@ -39,17 +39,18 @@ def vowels(word):
 
 def vowels_in_sent(sentence):
     # число гласных в предложении,
-    return [vowels(word[0]) for word in sentence]
+    return [vowels(word) for word in sentence]
 
 anna_sent = [words(sentence) for sentence in anna_sentences if len(words(sentence)) > 0]
 news_sent = [words(sentence) for sentence in news_sentences if len(words(sentence)) > 0]
 
-anna_data = [(sum(word_lens(sentence)),
-              different_letters(sentence),
-              sum(vowels_in_sent(sentence)),
-              np.median(word_lens(sentence)),
-              np.median(vowels_in_sent(sentence)))
+anna_data = [(sum(word_lens(sentence)),  # длина предложения в буквах,
+              different_letters(sentence),  # число различных букв в предложении,
+              sum(vowels_in_sent(sentence)),  # число гласных в предложении,
+              np.median(word_lens(sentence)),  # медиана числа букв в слове,
+              np.median(vowels_in_sent(sentence)))  # медиана числа гласных в слове.
              for sentence in anna_sent]
+
 news_data = [(sum(word_lens(sentence)),
               different_letters(sentence),
               sum(vowels_in_sent(sentence)),
@@ -60,10 +61,17 @@ news_data = [(sum(word_lens(sentence)),
 from matplotlib import pyplot as plt
 anna_data = np.array(anna_data)
 news_data = np.array(news_data)
-for i, j in itertools.combinations([0,1,2,3,4], 2):
-    print(i,j)
-    plt.figure()
-    plt.plot(anna_data[:,i], anna_data[:,j], 'sb',
-             news_data[:,i], news_data[:,j], 'og')
-    plt.show()
 
+# построила графики для всех комбинаций наших признаков:
+# for i, j in itertools.combinations([0,1,2,3,4], 2):
+#     print(i,j)
+#     plt.figure()
+#     plt.plot(anna_data[:,i], anna_data[:,j], 'sb',
+#              news_data[:,i], news_data[:,j], 'og')
+#     plt.show()
+
+plt.figure()
+plt.plot(anna_data[:,2], anna_data[:,3], 'sb',
+         news_data[:,2], news_data[:,3], 'og')
+plt.show()
+# лучше всего разделение получилось по осям 2 и 3 - число гласных в предложении, медиана числа букв в слове
