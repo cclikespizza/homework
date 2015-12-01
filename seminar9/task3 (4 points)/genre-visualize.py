901dc6f376013e40e8f5d784efe4ea1e0675aaa1
Elmira <cclikespizza@gmail.com>
2015-12-01 14:19:35 +0300

    final changes to pca-homework

--- seminar9/task3 (4 points)/genre-visualize.py
+++ seminar9/task3 (4 points)/genre-visualize.py
@@ -10,47 +10,51 @@ import itertools
 
 class Text:
     features = ['длина предложения в словах', 'длина предложения в буквах',
-                 'число различных букв в предложении', 'число гласных в предложении',
-                 'медиана числа букв в слове (медианная длина)', 'среднее число букв в слове (средняя длина слова)',
-                 'медиана числа гласных в слове', 'количество прилагательных',
-                 'количество существительных', 'количество глаголов',
-                 'количество наречий', 'количество местоимений']
+                'число различных букв в предложении', 'число гласных в предложении',
+                'медиана числа букв в слове (медианная длина)', 'среднее число букв в слове (средняя длина слова)',
+                'медиана числа гласных в слове', 'количество прилагательных',
+                'количество существительных', 'количество глаголов',
+                'количество наречий', 'количество местоимений']
 
     def __init__(self, fname):
         self.sentences = self.open_corpus(fname)
 
-    def open_corpus(self, fname):
-        parser = etree.HTMLParser()  # создаем парсер хтмл-страниц
-        # скармливаем парсеру xml, берем тэг body и все что внутри него
+    @staticmethod
+    def open_corpus(fname):
+        parser = etree.HTMLParser()
         tree = etree.parse(fname, parser).getroot()[0]
         return tree
 
     def make_features(self):
-        sentences = [self.sent_words(sent) for sent in self.sentences]
+        sentences = [self.sent_words(sent) for sent in self.sentences]  # каждое предложение -- массив слов
         data = [[len(sentence),  # длина предложения в словах,
                  sum(self.word_lens(sentence)),  # длина предложения в буквах,
-              self.different_letters(sentence),  # число различных букв в предложении,
-              sum(self.vowels_in_sent(sentence)),  # число гласных в предложении,
-              np.median(self.word_lens(sentence)),  # медиана числа букв в слове (медианная длина),
-              np.mean(self.word_lens(sentence)),  # среднее число букв в слове (средняя длина слова),
-              np.median(self.vowels_in_sent(sentence))]  # медиана числа гласных в слове.
-             for sentence in sentences]
+                 self.different_letters(sentence),  # число различных букв в предложении,
+                 sum(self.vowels_in_sent(sentence)),  # число гласных в предложении,
+                 np.median(self.word_lens(sentence)),  # медиана числа букв в слове (медианная длина),
+                 np.mean(self.word_lens(sentence)),  # среднее число букв в слове (средняя длина слова),
+                 np.median(self.vowels_in_sent(sentence))]  # медиана числа гласных в слове.
+                for sentence in sentences]
         pos_data = [self.pos_features(sent) for sent in self.sentences]  # массив про части речи
 
         return np.array([i+y for i,y in zip(data, pos_data)])
 
-    def pos_features(self, sent):
+    @staticmethod
+    def pos_features(sent):
         d = Counter([w[0].attrib['gr'].split('=')[0].split(',')[0] for w in sent if len(w) != 0])
         return [d['A'], d['S'], d['V'], d['ADV'], d['SPRO'] + d['APRO'] + d['ADVPRO']]
 
-    def sent_words(self, sent):
+    @staticmethod
+    def sent_words(sent):
         arr = [w.text.lower() for w in sent]
         return arr
 
-    def word_lens(self, sentence):
+    @staticmethod
+    def word_lens(sentence):
         return [len(i) for i in sentence]
 
-    def different_letters(self, sentence):
+    @staticmethod
+    def different_letters(sentence):
         russian_letters = 'ёйцукенгшщзхъфывапролджэячсмитьбю'
         # число различных букв в предложении,
         letters = set()
@@ -60,7 +64,8 @@ class Text:
                     letters.add(letter)
         return len(letters)
 
-    def vowels(self, word):
+    @staticmethod
+    def vowels(word):
         vowel_arr = 'ёуеэоаыяию'
         num = 0
         for letter in word:
@@ -74,34 +79,39 @@ class Text:
 
 
 def main():
+    print('Читаю тексты...')
     sonets = Text('corpus1.txt')
     anna = Text('corpus2.txt')
     news = Text('corpus3.txt')
 
+    print('Считаю характеристики...')
     sonets_data = sonets.make_features()
     anna_data = anna.make_features()
     news_data = news.make_features()
 
+    print('Использую метод главных компонент...')
     data = np.vstack((sonets_data, anna_data, news_data))
     p = mlab.PCA(data, True)
     N1 = len(sonets_data)
     N2 = len(anna_data)
 
-    main_features = sorted(zip(Text.features, p.Wt[0]), key=lambda pair: abs(pair[1]))[:3]
-    print(main_features)
+    print('Значимые признаки:')
+    main_features = sorted(zip(Text.features, p.s, range(12)), key=lambda pair: -abs(pair[1]))[:3]
+    print('\r\n'.join('   ' + par[0] + ' - ' + str(par[1]) for par in main_features))
 
-    for i, j in itertools.combinations(range(12), 2):
+    print('Рисую графики...')
+    plt.figure()
+    plt.plot(p.Y[:N1,0], p.Y[:N1,1], 'og', p.Y[N1:N2+N1,0], p.Y[N1:N2+N1,1], 'sb', p.Y[N2+N1:,0], p.Y[N2+N1:,1], 'xr')
+    plt.savefig('PCA-result.png')
+    plt.close()
+
+    for i, j in itertools.combinations(main_features, 2):
         plt.figure()
-        plt.plot(p.Y[:N1,i], p.Y[:N1,i], 'og', p.Y[N1:N2+N1,i], p.Y[N1:N2+N1,j], 'sb', p.Y[N2+N1:,j], p.Y[N2+N1:,j], 'xr')
-        plt.savefig('%s_vs_%s.png' % (Text.features[i], Text.features[j]))
+        plt.plot(data[:N1,i[2]], data[:N1,j[2]], 'og', data[N1:N2+N1,i[2]], data[N1:N2+N1,j[2]], 'sb', data[N2+N1:,j[2]], data[N2+N1:,j[2]], 'xr')
+        plt.savefig('%s_vs_%s.png' % (Text.features[i[2]], Text.features[j[2]]))
         plt.close()
 
-    with open('result.txt', 'w', encoding='utf-8') as res:
-        res.write('s: the actual eigenvalues of the decomposition\r\n')
-        res.write(str(p.s))
-        res.write('\r\n')
-        res.write('Wt: the weight vector\r\n')
-        res.write(str(p.Wt))
+    print('Ура! Конец!')
 
 
 if __name__ == "__main__":
